230fef17bac43eb747346f34ee8588cc9b34ea1b
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-09-06 14:55:51 +0600

    navbar and footer created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'nav-default': "#808080",
+        'nav-hover': "#000000",
+        'nav-active': "#0000ff",
       },
     },
   },
