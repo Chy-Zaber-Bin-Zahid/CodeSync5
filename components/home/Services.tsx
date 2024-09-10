e2d4d2b89c5e44152928b2ad328b67487d48ee35
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-09-10 17:00:32 +0600

    stagger added to cards

--- components/home/Services.tsx
+++ components/home/Services.tsx
@@ -21,12 +21,14 @@ function Services() {
             }
         );
         gsap.fromTo(
-            "#services-card",
+            ".service-card",
             { opacity: 0, y: "150" },
             {
-                duration: 1, opacity: 1, y: 0, delay: 0,
+                duration: 1, opacity: 1, y: 0, delay: 0, stagger: {
+                    each: 0.2,
+                },
                 scrollTrigger: {
-                    trigger: "#services-card",
+                    trigger: ".service-card", 
                 }
             }
         );
@@ -40,9 +42,9 @@ function Services() {
                         <p>We take digital experience to the next level</p>
                         <p className='text-center' >Our team of experts leverages the latest technologies to create innovative and scalable software solutions that help our clients improve their business processes, increase efficiency, and drive growth. We are committed to delivering high-quality, reliable, and secure software that meets the needs of our clients and their customers. With a wide range of services including.</p>
                     </div>
-                    <div id="services-card" className='w-full grid grid-cols-3 gap-4'>
+                    <div className='w-full grid grid-cols-3 gap-4'>
                         {services.map((item) => (
-                            <div key={item.name} className="p-4 flex flex-col gap-3 justify-center items-center rounded-sm shadow-services-card">
+                            <div key={item.name} className="service-card p-4 flex flex-col gap-3 justify-center items-center rounded-sm shadow-services-card">
                                 <h1>{item.name}</h1>
                                 <p>{item.description}</p>
                             </div>
