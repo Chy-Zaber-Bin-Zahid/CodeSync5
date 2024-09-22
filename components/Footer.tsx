f3379cf69f7216dfc3968232103404a7d9ca1839
Walid Ibne Hasan <walid.ibne.hasan@gmail.com>
2024-09-22 12:57:02 +0600

    Changed the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,48 +1,165 @@
-"use client"
+"use client";
+import { LinkedinLogo, FacebookLogo } from "phosphor-react";
+import React from "react";
 
-import Link from 'next/link';
-import { DeviceMobile, EnvelopeSimple, FacebookLogo, LinkedinLogo, MapPin } from 'phosphor-react';
-
-function Footer() {
+const Footer = () => {
   const currentYear = new Date().getFullYear();
+
+  const services = [
+    { name: "Web Development", href: "/services/web-development" },
+    { name: "Mobile Application", href: "/services/mobile-application" },
+    { name: "E-Commerce", href: "/services/e-commerce" },
+    { name: "AI Integration", href: "/services/ai-integration" },
+    { name: "UI/UX Design", href: "/services/ui-ux" },
+    { name: "Graphics Design", href: "/services/graphics-design" },
+  ];
+
+  const companyLinks = [
+    { name: "About", href: "/about" },
+    { name: "Meet the Team", href: "/meet-the-team" },
+  ];
+
+  const helpfulLinks = [
+    { name: "Contact", href: "/contact" },
+    { name: "FAQs", href: "/faqs" },
+  ];
+
+  const legalLinks = [
+    { name: "Accessibility", href: "/accessibility" },
+    { name: "Refund Policy", href: "/refund-policy" },
+    { name: "Terms & Conditions", href: "/terms-and-conditions" },
+  ];
+
+  const socialLinks = [
+    {
+      name: "Facebook",
+      icon: FacebookLogo,
+      href: "#",
+    },
+    {
+      name: "LinkedIn",
+      icon: LinkedinLogo,
+      href: "/linkedin",
+    },
+  ];
+
   return (
-    <div className="bg-footerBg text-white px-3 py-12 mt-4">
-      <div className='max-w-big-screen mx-auto grid grid-cols-3 gap-x-6 mq-765:grid-cols-1 mq-765:gap-y-8 mq-765:max-w-small-screen'>
-        <div className='flex flex-col justify-start items-start gap-1'>
-          <h1>logo</h1>
-          <p className='text-sm m-0'>Copyright © _____ | 2023-{currentYear}</p>
-          <p className='text-sm m-0'>All Rights Reserved.</p>
-        </div>
-        <div className='flex flex-col justify-start items-start gap-3'>
-          <div className='flex flex-col justify-center items-start gap-1'>
-            <h1 className='text-xl font-bold m-0' >Bangladesh</h1>
-            <p className='text-sm m-0 flex justify-center items-center gap-1'><MapPin size={16} weight="fill" /><Link href="/" className="text-white">____Address____</Link></p>
+    <footer className="bg-[#f0f4f8] dark:bg-gray-900">
+      <div className="mx-auto max-w-big-screen px-4 py-16 sm:px-6 lg:px-8">
+        <div className="lg:flex lg:gap-8">
+          <div className="mt-8 flex-1 grid grid-cols-2 gap-8 lg:mt-0 lg:grid-cols-4 lg:gap-y-16">
+            {/* Services Section */}
+            <div className="col-span-2 sm:col-span-1">
+              <p className="text-lg font-medium text-gray-900 dark:text-white">
+                Services
+              </p>
+              <ul className="pl-0 mt-6 space-y-4 text-md">
+                {services.map((service) => (
+                  <li key={service.name}>
+                    <a
+                      href={service.href}
+                      className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+                    >
+                      {service.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Company Section */}
+            <div className="col-span-2 sm:col-span-1">
+              <p className="font-medium text-gray-900 dark:text-white">
+                Company
+              </p>
+              <ul className="pl-0 mt-6 space-y-4 text-sm">
+                {companyLinks.map((link) => (
+                  <li key={link.name}>
+                    <a
+                      href={link.href}
+                      className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Helpful Links Section */}
+            <div className="col-span-2 sm:col-span-1">
+              <p className="font-medium text-gray-900 dark:text-white">
+                Helpful Links
+              </p>
+              <ul className="pl-0 mt-6 space-y-4 text-sm">
+                {helpfulLinks.map((link) => (
+                  <li key={link.name}>
+                    <a
+                      href={link.href}
+                      className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Legal Section */}
+            <div className="col-span-2 sm:col-span-1">
+              <p className="font-medium text-gray-900 dark:text-white">Legal</p>
+              <ul className="pl-0 mt-6 space-y-4 text-sm">
+                {legalLinks.map((link) => (
+                  <li key={link.name}>
+                    <a
+                      href={link.href}
+                      className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Social Media Section */}
+            <ul className="col-span-2 flex justify-start gap-6 lg:col-span-5 lg:justify-end">
+              {socialLinks.map((social) => (
+                <li key={social.name}>
+                  <a
+                    href={social.href}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="text-gray-700 transition hover:opacity-75 dark:text-gray-200"
+                  >
+                    <span className="sr-only">{social.name}</span>
+                    <social.icon size={24} />
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
-          <div className='flex flex-col justify-center items-start gap-1'>
-            <h1 className='text-xl font-bold m-0' >Phone</h1>
-            <p className='text-sm m-0 flex justify-center items-center gap-1'><DeviceMobile size={16} weight="fill" /><Link href="/" className="text-white">____PhoneNo____</Link></p>
-          </div>
         </div>
-        <div className='flex flex-col justify-start items-start gap-3'>
-          <div className='flex flex-col justify-center items-start gap-1'>
-            <h1 className='text-xl font-bold m-0' >Social</h1>
-            <p className='text-sm m-0 flex justify-center items-center gap-1'>
-              <Link href="/" className="text-white">
-                <FacebookLogo size={16} weight="fill" />
-              </Link>
-              <Link href="/" className="text-white">
-                <LinkedinLogo size={16} weight="fill" />
-              </Link>
+
+        <div className="mt-8 border-t border-gray-700 pt-8 dark:border-gray-800">
+          <div className="sm:flex sm:justify-between">
+            <p className="text-md text-gray-500 dark:text-gray-400">
+              &copy; {currentYear}. XYZ. All rights reserved.
             </p>
-          </div>
-          <div className='flex flex-col justify-center items-start gap-1'>
-            <h1 className='text-xl font-bold m-0' >Mail</h1>
-            <p className='text-sm m-0 flex justify-center items-center gap-1'><EnvelopeSimple size={16} weight="fill" /><Link href="/" className="text-white">____mail____</Link></p>
+
+            <p>
+              <a
+                href="/privacy-policy"
+                className="text-gray-500 transition hover:opacity-75 dark:text-gray-400"
+              >
+                Privacy Policy
+              </a>
+            </p>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
