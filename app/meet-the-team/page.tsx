34ffa5bcb5b11114e186391c9ba949ec5f1a5c70
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2024-09-23 01:25:57 +0600

    changes to design

--- app/meet-the-team/page.tsx
+++ app/meet-the-team/page.tsx
@@ -13,7 +13,7 @@ function page() {
                 <div className="w-full flex justify-center items-center mb-12">
                     <div className="relative w-fit group overflow-hidden">
                         <Image
-                            src="https://scontent.fdac178-1.fna.fbcdn.net/v/t1.6435-9/106193072_2701548266770528_2108268956625106134_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=nqBcAJTkvvkQ7kNvgEy-8fg&_nc_ht=scontent.fdac178-1.fna&_nc_gid=AaARRlK28GOaMHzXSCCziW3&oh=00_AYDBN4jxUQuvwGUTS2_KhUUZCDt6TUsPS_FFbvEhqPLPCQ&oe=6717C3A4"
+                            src="https://media.licdn.com/dms/image/v2/D5603AQFxSOYnlvHylw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1727032482264?e=1732752000&v=beta&t=m481D5h2Hp3hQ57RHIN-b1zni06wFEEMhfNyt2Pch_k"
                             width={370}
                             height={370}
                             objectFit="contain"
@@ -22,7 +22,8 @@ function page() {
                             placeholder="blur"
                             className="grayscale group-hover:grayscale-0 group-hover:scale-110 transition-all duration-500"
                         />
-                        <div className="absolute bottom-0 left-0 -translate-x-full group-hover:translate-x-0 bg-black opacity-70 w-full h-full text-white flex flex-col justify-center items-center transition-transform duration-300">
+                        <div className="absolute bottom-0 left-0 -translate-x-full group-hover:translate-x-0 bg-black opacity-30 w-full h-full transition-transform duration-500 z-10"></div>
+                        <div className="absolute bottom-0 left-0 -translate-x-full group-hover:translate-x-0 w-full h-full transition-all duration-500 z-20 flex flex-col justify-center items-center text-white">
                             <h1 className="text-lg font-semibold">Chowdhury Zaber Bin Zahid</h1>
                             <p>Co-Founder & CTO</p>
                         </div>
@@ -38,7 +39,8 @@ function page() {
                             placeholder="blur"
                             className="grayscale group-hover:grayscale-0 group-hover:scale-110 transition-all duration-500"
                         />
-                        <div className="absolute bottom-0 left-0 translate-x-full group-hover:translate-x-0 bg-black opacity-70 w-full h-full text-white flex flex-col justify-center items-center transition-transform duration-300">
+                        <div className="absolute bottom-0 left-0 translate-x-full group-hover:translate-x-0 bg-black opacity-30 w-full h-full transition-transform duration-500 z-10"></div>
+                        <div className="absolute bottom-0 left-0 translate-x-full group-hover:translate-x-0 w-full h-full transition-all duration-500 z-20 flex flex-col justify-center items-center text-white">
                             <h1 className="text-lg font-semibold">Walid Ibne Hasan</h1>
                             <p>Co-Founder & COO</p>
                         </div>
